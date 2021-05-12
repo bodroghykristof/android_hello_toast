44c80aa9e96cd94f1a1b3676c6a7332988203d6e
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-05-12 19:23:29 +0200

    preserve state at rotation

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hellotoast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String M_COUNT_KEY = "count";
 
     private Integer mCount = 0;
     private TextView showCount;
@@ -18,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.showCount = findViewById(R.id.show_count);
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(M_COUNT_KEY, mCount);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCount = savedInstanceState.getInt(M_COUNT_KEY);
+        showCount.setText(String.valueOf(mCount));
+    }
+
 
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
